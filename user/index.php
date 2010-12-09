dfffa76c8eb196caea3bee246dde36e4452ae713
dennis <dennis.iversen@gmail.com>
2010-12-09 15:53:37 +0100

    paging for user.

--- user/index.php
+++ user/index.php
@@ -2,10 +2,10 @@
 
 include_module('module_system_git');
 
-$num_rows = moduleSystemGit::getNumRows('user');
+$num_rows = moduleSystemGit::getNumRows();
 
 $pager = new pearPager($num_rows);
-
+$pager->from;
 viewModuleSystemGit::moduleFilterOption();
 $rows = moduleSystemGit::getAllUserModules($pager->from);
 viewModuleSystemGit::viewModules($rows);

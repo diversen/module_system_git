4a26228085d3fb8c2a4c43f369b77c0d7418cfaf
dennis <dennis.iversen@gmail.com>
2010-12-09 14:53:06 +0100

    correct paging.

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-include_once "pearPager.php";
-viewModuleSystemGit::moduleFilterOption();
-$rows = moduleSystemGit::getAllModules();
-viewModuleSystemGit::viewModules($rows);
 $num_rows = moduleSystemGit::getNumRows();
-
 $pager = new pearPager($num_rows);
+viewModuleSystemGit::moduleFilterOption();
+
+$rows = moduleSystemGit::getAllModules($pager->from);
+viewModuleSystemGit::viewModules($rows);
+
 $pager->pearPage();
--- user/index.php
+++ user/index.php
@@ -2,6 +2,13 @@
 
 include_module('module_system_git');
 
+$num_rows = moduleSystemGit::getNumRows('user');
+
+$pager = new pearPager($num_rows);
+
 viewModuleSystemGit::moduleFilterOption();
-$rows = moduleSystemGit::getAllUserModules();
+$rows = moduleSystemGit::getAllUserModules($pager->from);
 viewModuleSystemGit::viewModules($rows);
+
+
+$pager->pearPage();

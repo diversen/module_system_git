ca10ddeaa00645107367b20f96fcc7690341d793
dennis <dennis@dennis-desktop.(none)>
2011-02-21 14:58:50 +0100

    simplified auto compleate-

--- rpc.php
+++ rpc.php
@@ -9,16 +9,13 @@
 
 $db = new db();
 
-if(isset($_POST['queryString'])) {
-    $queryString = cos_htmlentities($_POST['queryString']);
+if(isset($_GET['term'])) {
+    $queryString = cos_htmlentities($_GET['term']);
     if(strlen($queryString) > 2) {
-        $query = "SELECT id, title FROM module_system_git WHERE title LIKE ". db::$dbh->quote("%" . $queryString . "%");
-        $rows = $db->selectQuery($query);
-        if($rows) {
-            foreach ($rows as $key => $val ) {
-                echo "<a href=\"/module_system_git/more/$val[id]\"><li onClick=\"fill(\''.$val[title].'\');\">$val[title]</li></a>";
-            }
-        }
+        $query = "SELECT id, title as value FROM module_system_git WHERE title LIKE ". db::$dbh->quote("%" . $queryString . "%");
+        $rows = $db->selectQuery($query);        
+        $json = json_encode($rows);
+        echo $json;
     }
 }
 
--- search.php
+++ search.php
@@ -1,78 +1,15 @@
 <script type="text/javascript">
-    // example found on:
-    // http://www.nodstrum.com/2007/09/19/autocompleter/
-	function lookup(inputString) {
-		if(inputString.length == 0) {
-			// Hide the suggestion box.
-			$('#suggestions').hide();
-		} else {
-			$.post("/module_system_git/rpc", {queryString: ""+inputString+""}, function(data){
-				if(data.length > 0) {
-					$('#suggestions').show();
-					$('#autoSuggestionsList').html(data);
-				}
-			});
-		}
-	} // lookup
-	
-	function fill(thisValue) {
-		$('#inputString').val(thisValue);
-		setTimeout("$('#suggestions').hide();", 200);
-	}
+$(function() {
+
+            $("#auto").autocomplete({
+                source: "/module_system_git/rpc",
+                minLength: 2,
+                select: function(event, ui) {
+                    var redirect = ui.item.id;
+                    location.href = "/module_system_git/more/" +redirect;   
+                }
+            });
+        });
+
 </script>
-
-<style type="text/css">
-
-
-	.suggestionsBox {
-		position: relative;
-		left: 0px;
-		margin: 10px 0px 0px 0px;
-		width: 200px;
-
-		-moz-border-radius: 7px;
-		-webkit-border-radius: 7px;
-		border: 2px solid #000;	
-
-	}
-	
-	.suggestionList {
-		margin: 0px;
-		padding: 0px;
-	}
-	
-	.suggestionList li {
-		
-		margin: 0px 0px 3px 0px;
-		padding: 3px;
-		cursor: pointer;
-                list-style:none;
-	}
-	
-	.suggestionList li:hover {
-		background-color: #659CD8;
-	}
-</style>
-
-
-
-
-	<div>
-		<form>
-			<div>
-				Type your county:
-				<br />
-				<input type="text" size="30" value="" id="inputString" onkeyup="lookup(this.value);" onblur="fill();" />
-			</div>
-
-			
-			<div class="suggestionsBox" id="suggestions" style="display: none;">
-
-				<div class="suggestionList" id="autoSuggestionsList">
-					&nbsp;
-				</div>
-			</div>
-		</form>
-	</div>
-
-
+<input type="text" id="auto" />
